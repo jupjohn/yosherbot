0d1b1e847f2b2528769feac95a374eaf2cc0fcb9
James Upjohn <jammehcow@jammehcow.co.nz>
2021-01-05 20:44:29 +1300

    Create basic .NET 5 console app

---
+++ Jammehcow.YosherBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jammehcow.YosherBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
