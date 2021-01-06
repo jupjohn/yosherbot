7c0ceeb2ec97ebbc89bd616ca8d8ba27f2840c65
James Upjohn <jammehcow@jammehcow.co.nz>
2021-01-06 21:28:05 +1300

    Test bot token scenarios

---
+++ src/Jammehcow.YosherBot.UnitTest/BotStartupTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Jammehcow.YosherBot.Console;
+using NUnit.Framework;
+
+namespace Jammehcow.YosherBot.UnitTest
+{
+    [TestFixture]
+    public class BotStartupTests
+    {
+        [Test]
+        public void ShouldFactoryThrow_WhenBotTokenMissing()
+        {
+            TestHelper.SetBotToken(null);
+            Assert.Throws(typeof(ArgumentException), () => BotStartup.CreateDefaultBot());
+        }
+
+        [Test]
+        public void ShouldFactoryReturn_WhenBotTokenProvided()
+        {
+            TestHelper.SetBotToken("abc");
+            Assert.DoesNotThrow(() => BotStartup.CreateDefaultBot());
+        }
+    }
+}
