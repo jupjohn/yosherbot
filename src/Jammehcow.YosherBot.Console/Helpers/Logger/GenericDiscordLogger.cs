f7b7da413155cb3bf38ebd968c9a58c2f1e42747
James Upjohn <jammehcow@jammehcow.co.nz>
2021-01-08 16:31:09 +1300

    Create a generic Discord.NET logger

---
+++ src/Jammehcow.YosherBot.Console/Helpers/Logger/GenericDiscordLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+
+namespace Jammehcow.YosherBot.Console.Helpers.Logger
+{
+    public class GenericDiscordLogger : IDiscordLogger
+    {
+        public Task HandleLogEventAsync(LogMessage logMessage)
+        {
+            // TODO: handle log levels
+            var logLevelLiteral = Enum.GetName(typeof(LogSeverity), logMessage.Severity) ?? nameof(LogSeverity.Info);
+            var messageParts = EnvironmentsHelper.IsDevelopment()
+                ? new[] {$"{logLevelLiteral}\t", logMessage.Source, logMessage.Message}
+                : new[] {$"{logLevelLiteral}\t", logMessage.Message};
+
+            System.Console.WriteLine(string.Join("-", messageParts));
+            if (logMessage.Exception != null)
+                System.Console.WriteLine(logMessage.Exception.ToString());
+
+            return Task.CompletedTask;
+        }
+    }
+}
