7c8f024d6b9338b48b9f4f7e3fc4e691d9229722
James Upjohn <jammehcow@jammehcow.co.nz>
2021-01-12 12:47:08 +1300

    Register command service in registration module

---
+++ src/Jammehcow.YosherBot.Console/ServiceRegistrationModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using Discord.Commands;
+
+namespace Jammehcow.YosherBot.Console
+{
+    // ReSharper disable once UnusedType.Global
+    public class ServiceRegistrationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterType<CommandService>()
+                .SingleInstance();
+        }
+    }
+}
