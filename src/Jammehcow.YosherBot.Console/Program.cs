a0b1d4f2460fe3f61cdee96d18a69d6007595cae
James Upjohn <jammehcow@jammehcow.co.nz>
2021-01-12 12:48:48 +1300

    Register bot startup as hosted service

--- src/Jammehcow.YosherBot.Console/Program.cs
+++ src/Jammehcow.YosherBot.Console/Program.cs
@@ -1,7 +1,14 @@
-﻿namespace Jammehcow.YosherBot.Console
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Jammehcow.YosherBot.Console
 {
     public static class Program
     {
-        public static void Main() => BotStartup.CreateDefaultBot().Run().GetAwaiter().GetResult();
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services.AddHostedService<BotStartup>());
     }
 }
