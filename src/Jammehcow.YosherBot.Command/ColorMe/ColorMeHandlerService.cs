766895b51ce5f82166f6aa6addacd07e9802ee84
James Upjohn <jammehcow@jammehcow.co.nz>
2021-02-27 12:49:47 +1300

    Add logging to colorme command

--- src/Jammehcow.YosherBot.Command/ColorMe/ColorMeHandlerService.cs
+++ src/Jammehcow.YosherBot.Command/ColorMe/ColorMeHandlerService.cs
@@ -28,23 +28,41 @@ namespace Jammehcow.YosherBot.Command.ColorMe
         // ReSharper disable once UnusedMember.Global
         public async Task HandleColourSet([Summary("A full or shorthand hex code")] string hexCode)
         {
+            // TODO: help commands, use subclasses?
+            _logger.LogInformation("Handling colorme command call for user {User}", Context.User.ToString());
+
             var generatedRoleName = RoleNameHelper.GetRoleNameFromUserId(Context.User.Id,
                 _configuration["Module:ColorMe:RolePrefix"]);
 
+            _logger.LogInformation("Generated role name of {RoleName} for user {User}", generatedRoleName,
+                Context.User.ToString());
+
             if (!HexColourHelper.TryGetColorFromHexString(hexCode, out var color))
             {
+                _logger.LogWarning("User {User} entered invalid hex code {HexCode}", Context.User.ToString(), hexCode);
                 await ReplyAsync($"Your hex code ``{hexCode}`` was invalid. Try something like #00FF00 or #abc");
                 return;
             }
 
+            _logger.LogInformation("Parsed hex code {HexCode} as RGB value ({R}, {G}, {B})", hexCode, color.R,
+                color.G, color.B);
+
+
             // Attempt to resolve the role and create if it doesn't exist
             var resolvedRole = Context.Guild.Roles.SingleOrDefault(r => r.Name == generatedRoleName) ??
                                await CreateColorRoleAsync(Context.Guild, generatedRoleName);
+            _logger.LogInformation("Resolved role {RoleName} in guild {GuildId}", generatedRoleName, Context.Guild.Id);
 
             // Set the colour of the role to the one specified by the user
             var roleColor = new Optional<Color>(new Color(color.R, color.G, color.B));
+
+            // TODO: if this fails?
             var user = Context.Guild.GetUser(Context.User.Id);
             var userTopRolePosition = user.Roles.Max(r => r.Position);
+
+            _logger.LogInformation("Top role for GuildUser {GuildUserId} is at position {RolePosition}", user.Id,
+                userTopRolePosition);
+
             await resolvedRole.ModifyAsync(props =>
             {
                 props.Color = roleColor;
@@ -52,8 +70,17 @@ namespace Jammehcow.YosherBot.Command.ColorMe
                 props.Mentionable = false;
                 props.Position = userTopRolePosition + 1;
             });
-            await user.AddRoleAsync(resolvedRole);
+
+            _logger.LogInformation("Updated color and role position for role {RoleName}", generatedRoleName);
+
+            if (user.Roles.All(r => r.Name != resolvedRole.Name))
+            {
+                _logger.LogInformation("Adding role {RoleName} to user {UserID}", generatedRoleName, user.Id);
+                await user.AddRoleAsync(resolvedRole);
+            }
+
             await Context.Message.AddReactionAsync(new Emoji("\uD83D\uDC4D"));
+            _logger.LogInformation("Successfully handled colorme command for user {User}", user.ToString());
         }
 
         [Command("uncolorme")]
