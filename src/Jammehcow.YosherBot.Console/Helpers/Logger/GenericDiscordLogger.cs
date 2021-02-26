c3573eefb8a3778491c584d5820f580205b2cd4e
James Upjohn <jammehcow@jammehcow.co.nz>
2021-02-26 20:33:04 +1300

    Replace barebones Discord logger impl with ILogger

--- src/Jammehcow.YosherBot.Console/Helpers/Logger/GenericDiscordLogger.cs
+++ src/Jammehcow.YosherBot.Console/Helpers/Logger/GenericDiscordLogger.cs
@@ -1,23 +1,64 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Discord;
+using Microsoft.Extensions.Logging;
 
 namespace Jammehcow.YosherBot.Console.Helpers.Logger
 {
     public class GenericDiscordLogger : IDiscordLogger
     {
+        private readonly ILogger<GenericDiscordLogger> _logger;
+
+        public GenericDiscordLogger(ILogger<GenericDiscordLogger> logger)
+        {
+            _logger = logger;
+        }
+
         public Task HandleLogEventAsync(LogMessage logMessage)
         {
-            // TODO: handle log levels
-            var logLevelLiteral = Enum.GetName(typeof(LogSeverity), logMessage.Severity) ?? nameof(LogSeverity.Info);
-            logLevelLiteral = logLevelLiteral.ToUpperInvariant().PadRight(8);
-            var messageParts = EnvironmentsHelper.IsDevelopment()
-                ? new[] {$"[{logLevelLiteral}]", logMessage.Source, logMessage.Message}
-                : new[] {$"[{logLevelLiteral}]", logMessage.Message};
+            var isDevelopment = EnvironmentsHelper.IsDevelopment();
 
-            System.Console.WriteLine(string.Join(" - ", messageParts));
-            if (logMessage.Exception != null)
-                System.Console.WriteLine(logMessage.Exception.ToString());
+            switch (logMessage.Severity)
+            {
+                case LogSeverity.Critical:
+                    if (isDevelopment)
+                        _logger.LogCritical("Discord.NET ({Source}) - {Message}", logMessage.Source, logMessage.Message);
+                    else
+                        _logger.LogCritical("Discord.NET - {Message}", logMessage.Message);
+                    if (logMessage.Exception != null)
+                        _logger.LogError("Exception: {Exception}", logMessage.Exception);
+                    break;
+                case LogSeverity.Error:
+                    if (isDevelopment)
+                        _logger.LogError("Discord.NET ({Source}) - {Message}", logMessage.Source, logMessage.Message);
+                    else
+                        _logger.LogError("Discord.NET - {Message}", logMessage.Message);
+                    if (logMessage.Exception != null)
+                        _logger.LogError("Exception: {Exception}", logMessage.Exception);
+                    break;
+                case LogSeverity.Warning:
+                    if (isDevelopment)
+                        _logger.LogWarning("Discord.NET ({Source}) - {Message}", logMessage.Source, logMessage.Message);
+                    else
+                        _logger.LogWarning("Discord.NET - {Message}", logMessage.Message);
+                    if (logMessage.Exception != null)
+                        _logger.LogError("Exception: {Exception}", logMessage.Exception);
+                    break;
+                case LogSeverity.Info:
+                    if (isDevelopment)
+                        _logger.LogInformation("Discord.NET ({Source}) - {Message}", logMessage.Source, logMessage.Message);
+                    else
+                        _logger.LogInformation("Discord.NET - {Message}", logMessage.Message);
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    if (isDevelopment)
+                        _logger.LogDebug("Discord.NET ({Source}) - {Message}", logMessage.Source, logMessage.Message);
+                    break;
+                default:
+                    _logger.LogInformation("Discord.NET ({Source}) - {Message}", logMessage.Source, logMessage.Message);
+                    break;
+            }
 
             return Task.CompletedTask;
         }
