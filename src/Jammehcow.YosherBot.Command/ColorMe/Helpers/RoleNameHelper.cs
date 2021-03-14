8af5356f424b209612388ab9594cd0ce19944ad2
James Upjohn <jammehcow@jammehcow.co.nz>
2021-03-14 19:53:09 +1300

    Replace base 16 role name generator with base 64

--- src/Jammehcow.YosherBot.Command/ColorMe/Helpers/RoleNameHelper.cs
+++ src/Jammehcow.YosherBot.Command/ColorMe/Helpers/RoleNameHelper.cs
@@ -1,11 +1,22 @@
+using System;
+
 namespace Jammehcow.YosherBot.Command.ColorMe.Helpers
 {
     public static class RoleNameHelper
     {
+        /// <summary>
+        /// Creates a unique role name using the User's ID
+        /// </summary>
+        /// <param name="userId">The ulong ID of the Discord user</param>
+        /// <param name="prefix">The prefix of the role. If null or empty no hyphen will be prefixed</param>
+        /// <returns></returns>
         public static string GetRoleNameFromUserId(ulong userId, string prefix)
         {
-            var roleHexPart = userId.ToString("X").ToLowerInvariant();
-            return string.Join('-', prefix, roleHexPart);
+            var roleHexPart = Convert.ToBase64String(BitConverter.GetBytes(userId));
+
+            return string.IsNullOrWhiteSpace(prefix)
+                ? roleHexPart
+                : string.Join('-', prefix, roleHexPart);
         }
     }
 }
