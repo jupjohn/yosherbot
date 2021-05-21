bccb40efdff915c897e9f066a48f4883094ca6b0
James Upjohn <jammehcow@jammehcow.co.nz>
2021-05-21 21:09:30 +1200

    Register AdministrationOptions with AutoFac

--- src/Jammehcow.YosherBot.Console/ServiceRegistrationModule.cs
+++ src/Jammehcow.YosherBot.Console/ServiceRegistrationModule.cs
@@ -13,6 +13,7 @@ namespace Jammehcow.YosherBot.Console
 
             builder.RegisterOptionSection<GeneralOptions>();
             builder.RegisterOptionSection<ColorMeModuleOptions>();
+            builder.RegisterOptionSection<AdministrationOptions>();
         }
     }
 }
