eea8b7d2c6d404db8e04e451b0c0849796433858
James Upjohn <jammehcow@jammehcow.co.nz>
2021-05-21 20:59:48 +1200

    Create administration command module

---
+++ src/Jammehcow.YosherBot.Command/Administration/AdministrationCommandModule.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.Logging;
+
+namespace Jammehcow.YosherBot.Command.Administration
+{
+    public class AdministrationCommandModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly ILogger<AdministrationCommandModule> _logger;
+
+        public AdministrationCommandModule(ILogger<AdministrationCommandModule> logger)
+        {
+            _logger = logger;
+        }
+
+        // TODO: move to own module, don't commit me with a snowflake literal
+        [Command("disconnect")]
+        [Summary("Disconnects and shuts down the bot")]
+        public async Task Disconnect()
+        {
+            // TODO: replace with config reference
+            if (Context.User.Id != 187814169813188608)
+            {
+                _logger.LogInformation(
+                    "User {UserId} tried to use command 'quit' but did not have administrative rights to do so",
+                    Context.User.Id);
+                return;
+            }
+
+            await Context.Message.AddReactionAsync(new Emoji("\uD83D\uDC4D"));
+            await Context.Client.StopAsync();
+        }
+    }
+}
