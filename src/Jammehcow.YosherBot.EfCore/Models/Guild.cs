0fd1e3a03c5a6c28e05628cde31bf3aa65ea6b73
James Upjohn <jammehcow@jammehcow.co.nz>
2021-05-29 14:58:00 +1200

    Create Guild entity

---
+++ src/Jammehcow.YosherBot.EfCore/Models/Guild.cs
@@ -0,0 +1,25 @@
+using System;
+using Jammehcow.YosherBot.EfCore.Models.Base;
+
+namespace Jammehcow.YosherBot.EfCore.Models
+{
+    public class Guild : IEntity
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The snowflake ID of the guild
+        /// </summary>
+        public long GuildId { get; set; }
+
+        /// <summary>
+        /// The date this guild was added/joined
+        /// </summary>
+        public DateTime DateAdded { get; set; }
+
+        /// <summary>
+        /// The date this guild was left
+        /// </summary>
+        public DateTime DateRemoved { get; set; }
+    }
+}
