3c0e997129469280a1a4649b15ab0b327c70b3cf
James Upjohn <jammehcow@jammehcow.co.nz>
2021-05-30 20:24:06 +1200

    Internalise setters of UserColorRole entity

--- src/Jammehcow.YosherBot.EfCore/Models/UserColorRole.cs
+++ src/Jammehcow.YosherBot.EfCore/Models/UserColorRole.cs
@@ -11,46 +11,46 @@ namespace Jammehcow.YosherBot.EfCore.Models
         /// <summary>
         /// The Discord ID of the user this role is applied to
         /// </summary>
-        public ulong UserSnowflake { get; set; }
+        public ulong UserSnowflake { get; internal set; }
 
         /// <summary>
         /// The display name of the role
         /// </summary>
         [Required]
-        public string RoleDisplayName { get; set; } = null!;
+        public string RoleDisplayName { get; internal set; } = null!;
 
         /// <summary>
         /// The Discord ID of the Guild role
         /// </summary>
-        public ulong RoleSnowflake { get; set; }
+        public ulong RoleSnowflake { get; internal set; }
 
         /// <summary>
         /// The ID of the guild
         /// </summary>
-        public int GuildId { get; set; }
-        public virtual Guild? Guild { get; set; }
+        public int GuildId { get; internal set; }
+        public virtual Guild? Guild { get; internal set; }
 
         /// <summary>
         /// The hex code of the role's color
         /// </summary>
         [Required]
-        public string ColorHexCode { get; set; } = null!;
+        public string ColorHexCode { get; internal set; } = null!;
 
         /// <summary>
         /// The status of this color role
         /// </summary>
         [Required]
-        public int ColorRoleStatusId { get; set; }
-        public virtual ColorRoleStatus RoleStatus { get; set; } = null!;
+        public int ColorRoleStatusId { get; internal set; }
+        public virtual ColorRoleStatus RoleStatus { get; internal set; } = null!;
 
         /// <summary>
         /// The date this role was applied
         /// </summary>
-        public DateTime DateCreated { get; set; }
+        public DateTime DateCreated { get; internal set; }
 
         /// <summary>
         /// The date this role was applied
         /// </summary>
-        public DateTime DateRemoved { get; set; }
+        public DateTime DateRemoved { get; internal set; }
     }
 }
