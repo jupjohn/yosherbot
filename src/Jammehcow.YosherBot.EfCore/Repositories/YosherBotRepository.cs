cd2e192d339789a55e5d469d45f4adcfbb8f07d2
James Upjohn <jammehcow@jammehcow.co.nz>
2021-05-30 20:14:50 +1200

    Add UserColorRole fetching method to repository

--- src/Jammehcow.YosherBot.EfCore/Repositories/YosherBotRepository.cs
+++ src/Jammehcow.YosherBot.EfCore/Repositories/YosherBotRepository.cs
@@ -1,5 +1,6 @@
 using Jammehcow.YosherBot.EfCore.Models;
 using MayBee;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Jammehcow.YosherBot.EfCore.Repositories
@@ -21,6 +22,23 @@ namespace Jammehcow.YosherBot.EfCore.Repositories
         public Maybe<Guild> GetGuildBySnowflakeId(ulong snowflakeId)
         {
             return Context.Guilds.SingleAsMaybe(guild => guild.GuildSnowflake == snowflakeId);
+        }
+
+        /// <summary>
+        /// Attempt to get a user's color role using the user and guild's snowflake IDs
+        /// </summary>
+        /// <param name="userSnowflake">Discord's snowflake ID of the user</param>
+        /// <param name="guildSnowflake">Discord's snowflake ID of the guild</param>
+        /// <returns>A Maybe either containing the UserColorRole or none if not found</returns>
+        public Maybe<UserColorRole> GetUserColorRole(ulong userSnowflake, ulong guildSnowflake)
+        {
+            // TODO: clean up nullability of Guild as it'll be a runtime error instead of something the compiler
+            // can see
+            return Context.UserColorRoles
+                .Include(ucr => ucr.Guild)
+                .SingleAsMaybe(ucr =>
+                    ucr.Guild?.GuildSnowflake == guildSnowflake &&
+                    ucr.UserSnowflake == userSnowflake);
         }
     }
 }
