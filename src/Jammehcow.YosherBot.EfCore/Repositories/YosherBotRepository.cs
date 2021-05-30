00942c2800b904d7d074a18ce7781ded069d7fa1
James Upjohn <jammehcow@jammehcow.co.nz>
2021-05-30 16:26:39 +1200

    Add method to Repository for fetching Guilds by Discord ID

--- src/Jammehcow.YosherBot.EfCore/Repositories/YosherBotRepository.cs
+++ src/Jammehcow.YosherBot.EfCore/Repositories/YosherBotRepository.cs
@@ -1,3 +1,5 @@
+using Jammehcow.YosherBot.EfCore.Models;
+using MayBee;
 using Microsoft.Extensions.Logging;
 
 namespace Jammehcow.YosherBot.EfCore.Repositories
@@ -9,6 +11,16 @@ namespace Jammehcow.YosherBot.EfCore.Repositories
         public YosherBotRepository(ILogger<YosherBotRepository> logger, YosherBotContext dbContext) : base(dbContext)
         {
             _logger = logger;
+        }
+
+        /// <summary>
+        /// Attempt to fetch a single guild with the given Discord snowflake ID
+        /// </summary>
+        /// <param name="snowflakeId">Discord's snowflake ID of the Guild</param>
+        /// <returns>A Maybe either containing the Guild or none if not found</returns>
+        public Maybe<Guild> GetGuildBySnowflakeId(long snowflakeId)
+        {
+            return Context.Guilds.SingleAsMaybe(guild => guild.Id == snowflakeId);
         }
     }
 }
